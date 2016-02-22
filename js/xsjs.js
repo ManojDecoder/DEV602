80e16dab3625ddde0e79ca7afb7f0fad5d80bfdd
Thomas Jung <thomas.jung@sap.com>
2016-02-22 11:43:40 -0500

    Update for WebIDE

--- js/xsjs.js
+++ js/xsjs.js
@@ -1,23 +1,21 @@
 'use strict';
-var path = require('path');
-var xsjs = require('sap-xsjs');
-var xsenv = require('sap-xsenv');
-var port = process.env.PORT || 3000;
+var xsjs  = require("sap-xsjs");
+var xsenv = require("sap-xsenv");
+var port  = process.env.PORT || 3000;
 
 var options = xsjs.extend({
-  rootDir: path.join(__dirname, 'src'),
-  redirectUrl: '/index.xsjs',
-  port: port
-}, xsenv.getServices(
-	{
-		hana:{ tag:'hana' }, 
-	 	uaa:{ name:process.env.UAA_SERVICE_NAME }
-	}
-));
+	//anonymous : true, // remove to authenticate calls
+	redirectUrl : "/index.xsjs"
+});
 
-console.log(JSON.stringify(options));
+// configure HANA
+options = xsjs.extend(options, xsenv.getServices({ hana: {tag: "hana"} }));
 
+// configure UAA
+options = xsjs.extend(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
 
+// start server
 xsjs(options).listen(port);
-console.log('Using HANA on %s:%d', options.hana.host, options.hana.port);
+
+console.log("Server listening on port %d", port);
 
